9ed0ede013add084bae005f70ed30e79b835fa2b
sada498 <sada.y@outlook.com>
2021-01-18 15:41:35 +1100

    v 0.1

---
+++ src/main/java/com/sada/SpringCloudGCPCloud/SQLDB/SpringCloudGcpCloudSqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.sada.SpringCloudGCPCloud.SQLDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGcpCloudSqlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGcpCloudSqlDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sada/SpringCloudGCPCloud/SQLDB/SpringCloudGcpCloudSqlDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sada.SpringCloudGCPCloud.SQLDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGcpCloudSqlDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
